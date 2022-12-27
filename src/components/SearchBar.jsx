74e811c8d17584f91cbb6cc00a46115bc6dd2786
younghwan12 <dowk3432@gmail.com>
2022-12-27 10:24:46 +0900

    2022.12.27

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsSearch } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+
 
 const SearchBar = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onKeyPress = (e) => {
+    if (e.charCode === 13) {
+      handleSearch();
+    }
+  };
+
+  const handleSearch = () => {
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  };
+
   return (
-    <div className="search">
+    <div className="search" onKeyPress={onKeyPress}>
       <label className="glass" htmlFor="searchInput">
         <BsSearch />
       </label>
@@ -13,6 +32,8 @@ const SearchBar = () => {
         className="input__search"
         placeholder="축구 리그를 검색하세요!"
         title="검색"
+        value={searchTerm || ''}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
     </div>
   )
--- src/components/SearchConts.jsx
+++ src/components/SearchConts.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { fetchAPI } from '../utils/fetchAPI'
+import { Videos } from './'
 
 const SearchConts = () => {
-  return <div>SearchConts</div>
+  const [videos, setVideos] = useState(null);
+  const { searchKeyword } = useParams();
+
+  useEffect(() => {
+    fetchAPI(`search?part=snippet&q=${searchKeyword}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchKeyword]);
+
+  console.log(searchKeyword)
+
+  return (
+    <main id="main">
+      <div className='searchCont'>
+        <div className="result"><span>{searchKeyword}</span>를 검색하였습니다.</div>
+        <Videos videos={videos}/>
+      </div>
+    </main>
+    )
 }
 
 export default SearchConts
